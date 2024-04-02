6b78d33a7215d5ac7772461e7228d6bbb6042328
firyalshafa <shafafiryal782@gmail.com>
2024-04-02 17:00:27 +0800

    membuat variable / struct mahasiswa

--- pertemuanke6/pertemuanke6.cpp
+++ pertemuanke6/pertemuanke6.cpp
@@ -4,10 +4,16 @@
 #include <iostream>
 using namespace std;
 
+struct Mahasiswa {
+    int nim;
+    int nama;
+    int alamat;
+    int umur;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
 
 
