c72da77b4121fd5835d18e965f6e487caca31b65
firyalshafa <shafafiryal782@gmail.com>
2024-04-02 17:38:17 +0800

    memnambahkan detail pada sturtur alamat

--- pertemuanke6/6-kedua.cpp
+++ pertemuanke6/6-kedua.cpp
@@ -13,4 +13,30 @@ struct Mahasiswa {
     int umur;
 };
 
+int main()
+{
+    Mahasiswa mhs;
+    cout << "nomor mahasiswa :";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa :";
+    cin >> mhs.nama;
+
+    cout << " alamat mahasiswa :";
+    cout << "/t nama desa :";
+    cin >> mhs.alamat.desa;
+    cout << "/t nama kota :";
+    cin >> mhs.alamat.kota;
+
+    cout << " umur mhasiswa :";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n nim :" << mhs.nim;
+    cout << "\n nama :" << mhs.nama;
+    cout << "\n alamat :";
+    cout << "\n desa :" << mhs.alamat.desa;
+    cout << "\n kota : " << mhs.alamat.kota;
+    cout << "\n umur : " << mhs.umur;
+}
+
 
