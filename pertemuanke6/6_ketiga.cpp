c4c2ee09b954ecd61118e6737cc9cf3a8144796a
firyalshafa <shafafiryal782@gmail.com>
2024-04-02 18:43:15 +0800

    menambahkan getline

--- pertemuanke6/6_ketiga.cpp
+++ pertemuanke6/6_ketiga.cpp
@@ -12,3 +12,29 @@ struct Mahasiswa {
     detailalamat alamat;
     int umur;
 };
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "nomor mahasiswa :";
+    getline (cin, mhs.nim);
+    cout << "nama mahasiswa :";
+    getline(cin, mhs.nama);
+
+    cout << " alamat mahasiswa :";
+    cout << "/t nama desa :";
+    cin >> mhs.alamat.desa;
+    cout << "/t nama kota :";
+    cin >> mhs.alamat.kota;
+
+    cout << " umur mhasiswa :";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n nim :" << mhs.nim;
+    cout << "\n nama :" << mhs.nama;
+    cout << "\n alamat :";
+    cout << "\n desa :" << mhs.alamat.desa;
+    cout << "\n kota : " << mhs.alamat.kota;
+    cout << "\n umur : " << mhs.umur;
+}
